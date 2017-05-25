3f39b0a730bd943524b991df842159bc8b72d5ce
iwata-4403 <yoshimasa0033@gmail.com>
2017-05-25 11:51:43 +0900

    Add files via upload

--- Integration/statics.ino
+++ Integration/statics.ino
@@ -77,9 +77,10 @@ double rad_ave(const int array_num, double* value) {
   Vector2D vector_ave;
 
   //角度を北から時計回りにとったxy座標上の点にする
+  /*ここも引数がdegだからdeg2radだよね？*/
   for (i = 0; i < array_num; i++) {
-    vector[i].x = cos(rad2deg(value[i]));
-    vector[i].y = sin(rad2deg(value[i]));
+    vector[i].x = cos(deg2rad(value[i]));
+    vector[i].y = sin(deg2rad(value[i]));
   }
 
   //点の重心をとる(平均みたいになります)
@@ -91,8 +92,12 @@ double rad_ave(const int array_num, double* value) {
   vector_ave.y = vector_sum.y / array_num;
 
   //重心の角度を北から時計回りで出します
-  ave = atan2(vector_ave.x, vector_ave.y);  //東から
+  ave = atan2(vector_ave.y, vector_ave.x);  //東から
+  /*これってはatan2だからはじめからradだからrad2degで逆だよね？*/
+  //一旦degになおしてから０～３６０に変換してradに戻す
+  ave = rad2deg(ave);
   ave = deg2rad(((int)ave) % 360);
+  
 
   return ave;
 }
