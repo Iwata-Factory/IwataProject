dbe1fa3a0a711049fe2b8bfb59ea2a77ed73dc4d
Toma0916 <yiyiaxpb@gmail.com>
2017-05-25 19:38:42 +0900

    途中

---
+++ StructTest/make_test.py
@@ -0,0 +1,66 @@
+# coding:utf8
+
+import os
+import random
+
+
+def go_suddenly(path, times):
+    f = open(path, "a")
+    f.write("  DRIVE go;\n  go.right1 = 0;\n  go.right2 = 1;\n  go.leght1 = 0;\n  go.leght2 = 1;\n  rover_degital(go);\n")
+    f.write("  delay(")
+    f.write(times)
+    f.write(");\n  go.right1 = 1;\n  go.right2 = 1;\n  go.leght1 = 1;\n  go.leght2 = 1;\n  rover_degital(go);\n")
+
+
+    f.write("  delay(2500);\n")
+    f.close()
+
+
+
+
+
+
+def main():
+    c = os.getcwd()  # カレントディレクトリを取得
+    source_str = 'abcdefghijklmnopqrstuvwxyz1234567890'
+    random.choice(source_str)
+    name = "".join([random.choice(source_str) for i in range(0, 6)])  # ファイル名をランダムに生成
+    print("機体走行テストファイルを作成します。")
+    print("テストファイル名は" + name + ".inoです。")
+    os.mkdir(os.path.join(c, name))
+    print("ディレクトリの用意が完了。")
+    f_path = os.path.join(c, name, name + ".ino")
+    f = open(f_path, "w")
+    print("inoファイルの作成が完了。")
+    print("書き込みを開始します。")
+    print("枠型のセットアップ")
+    f.write("#include <Wire.h>\n")
+    f.write("#include <SoftwareSerial.h>\n")
+    f.write("#define SERIAL_BAUDRATE 9600\n")
+    f.write("#define M1_1 8\n")
+    f.write("#define M1_2 9\n")
+    f.write("#define M2_1 10\n")
+    f.write("#define M2_2 11\n")
+    f.write("typedef struct {\n")
+    f.write("  int right1 = 0;\n")
+    f.write("  int right2 = 0;\n")
+    f.write("  int leght1 = 0;\n")
+    f.write("  int leght2 = 0;\n")
+    f.write("} DRIVE;\n")
+    f.write("void rover_degital(DRIVE drive) {\n  digitalWrite(M1_1, drive.right1);\n  digitalWrite(M1_2, drive.right2);\n")
+    f.write("  digitalWrite(M2_1, drive.leght1);\n  digitalWrite(M2_2, drive.leght2);\n}\n")
+    f.write("void  setup() {\n  Serial.begin(SERIAL_BAUDRATE);\n")
+    f.write("  pinMode(M1_1, OUTPUT);\n  pinMode(M1_2, OUTPUT);\n  pinMode(M2_1, OUTPUT);\n  pinMode(M2_2, OUTPUT);\n}\n")
+    f.write("void loop() {\n")
+    f.close()
+    print("工程のセットアップ")
+    go_suddenly(f_path, "3456")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
