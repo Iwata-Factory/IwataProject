0b5b9d6eee87b9c1126d1742a9722def52a10770
Toma0916 <yiyiaxpb@gmail.com>
2017-05-26 14:45:19 +0900

    走行試験自動生成

--- StructTest/make_test.py
+++ StructTest/make_test.py
@@ -4,6 +4,27 @@ import os
 import random
 
 
+def go_smoothly(path, times):
+    f = open(path, "a")
+    # 加速部
+    f.write("  for (int i = 1; i < 256; i++) {\n")
+    f.write("    go.right1 = 0;\n    go.right2 = i;\n    go.leght1 = 0;\n    go.leght2 = i;\n    rover_analog(go);\n")
+    f.write("    delay(40);\n  }\n")
+    # 直進部
+    f.write("  go.right1 = 0;\n  go.right2 = 1;\n  go.leght1 = 0;\n  go.leght2 = 1;\n  rover_degital(go);\n")
+    f.write("  delay(")
+    f.write(times)
+    f.write(");\n")
+    # 減速部
+    f.write("  for (int i = 255; i > 0; i--) {\n")
+    f.write("    go.right1 = 0;\n    go.right2 = i;\n    go.leght1 = 0;\n    go.leght2 = i;\n    rover_analog(go);\n")
+    f.write("    delay(40);\n  }\n")
+    # 停止部
+    f.write("  go.right1 = 1;\n  go.right2 = 1;\n  go.leght1 = 1;\n  go.leght2 = 1;\n  rover_degital(go);\n")
+    f.write("  delay(2500);\n")
+    f.close()
+
+
 def go_suddenly(path, times):
     f = open(path, "a")
     f.write("  go.right1 = 0;\n  go.right2 = 1;\n  go.leght1 = 0;\n  go.leght2 = 1;\n  rover_degital(go);\n")
@@ -13,6 +34,28 @@ def go_suddenly(path, times):
     f.write("  delay(2500);\n")
     f.close()
 
+
+def turn_right_smoothly(path, times):
+    f = open(path, "a")
+    # 加速部
+    f.write("  for (int i = 1; i < 256; i++) {\n")
+    f.write("    go.right1 = 0;\n    go.right2 = i;\n    go.leght1 = i;\n    go.leght2 = 0;\n    rover_analog(go);\n")
+    f.write("    delay(40);\n  }\n")
+    # 回転部
+    f.write("  go.right1 = 0;\n  go.right2 = 1;\n  go.leght1 = 1;\n  go.leght2 = 0;\n  rover_degital(go);\n")
+    f.write("  delay(")
+    f.write(times)
+    f.write(");\n")
+    # 減速部
+    f.write("  for (int i = 255; i > 0; i--) {\n")
+    f.write("    go.right1 = 0;\n    go.right2 = i;\n    go.leght1 = i;\n    go.leght2 = 0;\n    rover_analog(go);\n")
+    f.write("    delay(40);\n  }\n")
+    # 停止部
+    f.write("  go.right1 = 1;\n  go.right2 = 1;\n  go.leght1 = 1;\n  go.leght2 = 1;\n  rover_degital(go);\n")
+    f.write("  delay(2500);\n")
+    f.close()
+
+
 def turn_right_suddenly(path, times):
     f = open(path, "a")
     f.write("  go.right1 = 0;\n  go.right2 = 1;\n  go.leght1 = 1;\n  go.leght2 = 0;\n  rover_degital(go);\n")
@@ -21,6 +64,30 @@ def turn_right_suddenly(path, times):
     f.write(");\n  go.right1 = 1;\n  go.right2 = 1;\n  go.leght1 = 1;\n  go.leght2 = 1;\n  rover_degital(go);\n")
     f.write("  delay(2500);\n")
     f.close()
+
+
+def turn_leght_smoothly(path, times):
+    f = open(path, "a")
+    # 加速部
+    f.write("  for (int i = 1; i < 256; i++) {\n")
+    f.write(
+        "    go.right1 = i;\n    go.right2 = 0;\n    go.leght1 = 0;\n    go.leght2 = i;\n    rover_analog(go);\n")
+    f.write("    delay(40);\n  }\n")
+    # 回転部
+    f.write("  go.right1 = 1;\n  go.right2 = 0;\n  go.leght1 = 0;\n  go.leght2 = 1;\n  rover_degital(go);\n")
+    f.write("  delay(")
+    f.write(times)
+    f.write(");\n")
+    # 減速部
+    f.write("  for (int i = 255; i > 0; i--) {\n")
+    f.write(
+        "    go.right1 = i;\n    go.right2 = 0;\n    go.leght1 = 0;\n    go.leght2 = i;\n    rover_analog(go);\n")
+    f.write("    delay(40);\n  }\n")
+    # 停止部
+    f.write("  go.right1 = 1;\n  go.right2 = 1;\n  go.leght1 = 1;\n  go.leght2 = 1;\n  rover_degital(go);\n")
+    f.write("  delay(2500);\n")
+    f.close()
+
 
 def turn_leght_suddenly(path, times):
     f = open(path, "a")
@@ -61,6 +128,8 @@ def main():
     f.write("} DRIVE;\n")
     f.write("void rover_degital(DRIVE drive) {\n  digitalWrite(M1_1, drive.right1);\n  digitalWrite(M1_2, drive.right2);\n")
     f.write("  digitalWrite(M2_1, drive.leght1);\n  digitalWrite(M2_2, drive.leght2);\n}\n")
+    f.write("void rover_analog(DRIVE drive) {\n  analogWrite(M1_1, drive.right1);\n  analogWrite(M1_2, drive.right2);\n")
+    f.write("  analogWrite(M2_1, drive.leght1);\n  analogWrite(M2_2, drive.leght2);\n}\n")
     f.write("void  setup() {\n  Serial.begin(SERIAL_BAUDRATE);\n")
     f.write("  pinMode(M1_1, OUTPUT);\n  pinMode(M1_2, OUTPUT);\n  pinMode(M2_1, OUTPUT);\n  pinMode(M2_2, OUTPUT);\n}\n")
     f.write("void loop() {\n")
@@ -77,14 +146,89 @@ def main():
         print("4:急に右旋回")
         print("5:滑らかに左旋回")
         print("6:急に左旋回")
-        print("7:もう何もしない。")
+        print("7:ランダムに動きを生成")
+        print("8:生成を終了して保存。")
         command = input("数字で入力して下さい。\n")
-        if command == "2":
+        if command == "1":
+            t = input("何ミリ秒行いますか？\n")
+            go_smoothly(f_path, t)
+            print("")
+            print("書き込み完了")
+            print("")
+        elif command == "2":
             t = input("何ミリ秒行いますか？\n")
             go_suddenly(f_path, t)
+            print("")
+            print("書き込み完了")
+            print("")
+        elif command == "3":
+            t = input("何ミリ秒行いますか？\n")
+            turn_right_smoothly(f_path, t)
+            print("")
+            print("書き込み完了")
+            print("")
+        elif command == "4":
+            t = input("何ミリ秒行いますか？\n")
+            turn_right_suddenly(f_path, t)
+            print("")
+            print("書き込み完了")
+            print("")
+        elif command == "5":
+            t = input("何ミリ秒行いますか？\n")
+            turn_leght_smoothly(f_path, t)
+            print("")
+            print("書き込み完了")
+            print("")
+        elif command == "6":
+            t = input("何ミリ秒行いますか？\n")
+            turn_right_smoothly(f_path, t)
+            print("")
+            print("書き込み完了")
+            print("")
         elif command == "7":
+            print("")
+            print("乱数を生成します。")
+            num = str(random.randint(1, 6))
+            print("ナンバー" + num + "の動きを生成")
+            num2 = random.randint(1, 3)
+            if num2 == 1:
+                t = "1000"
+                print("動作時間は" + str(t) + "秒")
+            elif num2 == 2:
+                t = "3000"
+                print("動作時間は" + str(t) + "秒")
+            else:
+                t = "5000"
+                print("動作時間は" + str(t) + "秒")
+            print("")
+            if num == "1":
+                go_smoothly(f_path, t)
+            elif num == "2":
+                go_suddenly(f_path, t)
+            elif num == "3":
+                turn_right_smoothly(f_path, t)
+            elif num == "4":
+                turn_right_suddenly(f_path, t)
+            elif num == "5":
+                turn_leght_smoothly(f_path, t)
+            elif num == "6":
+                turn_right_smoothly(f_path, t)
+            turn_right_smoothly(f_path, t)
+            print("書き込み完了")
+            print("")
+        elif command == "8":
+            print("")
+            print("書き込みを終了して保存します。")
+            print("")
+            f = open(f_path, "a")
+            f.write("}\n")
+            f.close()
             break
-
+        else:
+            print("")
+            print("不適切な値が入力されました。")
+            print("再度入力して下さい.")
+            print("")
 
 if __name__ == "__main__":
     main()
