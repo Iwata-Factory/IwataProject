08f29dfb73d0494a310d6fb75a5e15d2a064829c
Toma0916 <yiyiaxpb@gmail.com>
2017-05-30 10:06:17 +0900

    コメント追加

--- SDtest/test2/SD.ino
+++ SDtest/test2/SD.ino
@@ -79,6 +79,8 @@ int write_gps_sd(struct GPS gps) {
   return 0; // 失敗を返す
 }
 
+// 第一引数:値を入れる構造体の配列
+// 第二引数:何個の値を入れるか
 int read_ac_sd(AC ac[100], int num) {
 
   // 0.0で初期化
@@ -89,45 +91,61 @@ int read_ac_sd(AC ac[100], int num) {
   }
 
   int i = 0; // 試行回数記録用
-  Serial.println("加速度を読みます。");
+  Serial.println("加速度を読みとります。");
   while (i < 30) { // 30回SDカードを開けなかったら諦める
+    
     File dataFile = SD.open("aclog.txt", FILE_READ);
+    
     if (dataFile) { // ファイルが開けたときの処理
-      int now_pos = dataFile.size();
-      static int back_num = 1;
-      dataFile.seek(now_pos);
-      for (int j = 0; j < num; j++) {
+      int now_pos = dataFile.size(); // now_pos は自身の位置を表す
+      static int back_num = 1; // 操作ごとに調べる位置をいくつ戻すか
+      dataFile.seek(now_pos); // 位置移動
+      
+      for (int j = 0; j < num; j++) { // num回分のデータを取る
         while (1) {
-          dataFile.seek(now_pos - back_num);
+          
+          dataFile.seek(now_pos - back_num); // 調べる位置を一つ戻す
+          
           String line = dataFile.readStringUntil('\r');
-          if (line == "*") {
+          
+          if (line == "*") { // もし*と書いてある行に達したらその下の三行を読む
             String x = dataFile.readStringUntil('\r');
             // Serial.println(x);
-            ac[j].x = x.toDouble();
             String y = dataFile.readStringUntil('\r');
             //   Serial.println(y);
-            ac[j].y = y.toDouble();
             String z = dataFile.readStringUntil('\r');
-            //   Serial.println(z);
-            ac[j].z = z.toDouble();
-            now_pos -= back_num;
-            if (now_pos == 0) {
-              dataFile.close();
-              return (-1 * (j + 1)); // 途中で終了したがj番目まで取れた
+            if ((x != "*") & (y != "*") & (z != "*")) { // 値が揃っていたなら代入、そうでないなら0のまま
+              ac[j].x = x.toDouble();
+              ac[j].y = y.toDouble();
+              ac[j].z = z.toDouble();
+            } else {
+              ac[j].x = 0.0;
+              ac[j].y = 0.0;
+              ac[j].z = 0.0;
             }
-            break;
+
+            //   Serial.println(z);
+            now_pos -= back_num; //  調べる位置を戻す
+            
+            if (now_pos == 0) { // ファイルの先頭に達してしまった場合
+              dataFile.close();
+              return (-1 * (j + 1)); // 途中で終了したがj+1番目まで取れたことを返す(負の値で)
+            }
+            
+            break; // j + 1 回目に移行
+            
           } else {
             now_pos -= back_num;
-            if (now_pos == 0) {
+            if (now_pos == 0) {  // ファイルの先頭に達してしまった場合
               dataFile.close();
-              return (-1 * j); // 途中で終了したがj番目まで取れた
+              return (-1 * j); // 途中で終了したがj番目まで取れたことを返す(負の値で)
             }
           }
           continue;
         }
       }
       dataFile.close();
-      return 1; // 成功を返す
+      return 1; // 要求された処理が完了したことを返す
     } else {
       Serial.println("ファイルオープンに失敗");
       i += 1;
