a39054706da9352eeb9b9ea1045b582416dbc27e
iwata-4403 <yoshimasa0033@gmail.com>
2017-06-02 11:43:15 +0900

    これでGPSの生データみれます

---
+++ GPS_test/GPS_test.ino
@@ -0,0 +1,24 @@
+#include <SoftwareSerial.h>
+
+#define PIN_GPS_Rx 50
+#define PIN_GPS_Tx 53
+
+#define SERIAL_BAUDRATE 9600
+#define GPSBAUDRATE 9600
+
+SoftwareSerial sGps(PIN_GPS_Rx, PIN_GPS_Tx);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(SERIAL_BAUDRATE);
+  Serial.println("Software Serial Test Start!"); 
+
+  sGps.begin(GPSBAUDRATE);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (sGps.available()) {  
+    Serial.write(sGps.read());
+  }
+}
