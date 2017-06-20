780dbafe9417e9ffd7013d24fbc24f30838a790a
Toma0916 <yiyiaxpb@gmail.com>
2017-06-20 18:03:58 +0900

    調整

--- INTEGRATION_TYPE2/DEFINE.h
+++ INTEGRATION_TYPE2/DEFINE.h
@@ -12,7 +12,7 @@
 #define LATITUDE_MAXIMUM 45  //緯度の最大値
 #define LONGITUDE_MINIMUM 133  //経度の最小値
 #define LONGITUDE_MAXIMUM 143  //経度の最大値
-#define GPSBAUDRATE 9600 //シリアル通信のデータ送信レートを9600bpsに定義するための定数(GPSとArduino)
+#define GPSBAUDRATE 14400 //シリアル通信のデータ送信レートを9600bpsに定義するための定数(GPSとArduino)
 #define READBUFFERSIZE  (256)
 #define DELIMITER   (",")  // 区切り文字定数
 
