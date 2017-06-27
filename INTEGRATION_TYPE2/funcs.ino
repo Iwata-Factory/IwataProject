6e8c5592c702be2f61315d7f5cbe04bb44f9e05d
iwata-4403 <yoshimasa0033@gmail.com>
2017-06-27 17:57:04 +0900

    Add files via upload

--- INTEGRATION_TYPE2/funcs.ino
+++ INTEGRATION_TYPE2/funcs.ino
@@ -52,7 +52,7 @@ int AnalyzeLineString( char szLineString[], GPS* gps) {
      通信ができていても生データが通信状況悪いとVになります
      これが出る場合は屋外とか通信状況よくなるようにしてください
   */
-  if ( strncmp(*gps_status, 'V', 1 ) == 0) {
+  if ( strncmp(*gps_status, "V", 1 ) == 0) {
     xbee_uart( dev, "BAD communicatin condition of gps...\r");
   }
   gps->utc = atof(psz_utc);
@@ -254,7 +254,7 @@ int AnalyzeLineString_al( char szLineString[], double* altitude) {
      通信ができていても生データが通信状況悪いとVになります
      これが出る場合は屋外とか通信状況よくなるようにしてください
   */
-  if ( strncmp(*psz_q, '0', 1 ) == 0) {
+  if ( strncmp(*psz_q, "0", 1 ) == 0) {
     xbee_uart( dev, "BAD communicatin condition of gps...\r");
     return 0;
   }
