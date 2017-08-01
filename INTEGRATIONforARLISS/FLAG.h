b981870ea4eccd82e192997971ac3eb7c84da39e
Toma0916 <yiyiaxpb@gmail.com>
2017-08-01 16:20:31 +0900

    調整

--- INTEGRATIONforARLISS/FLAG.h
+++ INTEGRATIONforARLISS/FLAG.h
@@ -22,7 +22,7 @@
 #define DANGER_AREA_FRAG 0 //危険エリア脱出処理を行うならば1.行わないならば0.
 #define SD_LOG_FLAG 1  // SDに書き込みを行うなら1.行わないなら0.
 #define SPHERE_FLAG 1  // 1で球面三角法を用いる.0では平面として処理.(球面三角法推奨
-#define GPS_GET_FLAG 0 // 0の時GPSを取らない
+#define GPS_GET_FLAG 1// 0の時GPSを取らない
 #define PI_FLAG 1  // 1の時PI制御で直進する。
 
 // テスト用フラグ
