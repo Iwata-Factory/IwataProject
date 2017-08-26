afbf1d2c4b4ac538fec01fd23e833cc787ced31b
Toma0916 <yiyiaxpb@gmail.com>
2017-08-26 20:17:16 +0900

    出力調整追加

--- INTEGRATIONforARLISS/status5.ino
+++ INTEGRATIONforARLISS/status5.ino
@@ -23,12 +23,26 @@ int status5(ROVER *rover) {
   float d_lat = 0;
   float d_lng = 0;
   float d_time = 0;
-  
-  
-  do {
 
+  DRIVE go; //DRIVE型の宣言
+  // 初期化
+
+  go.right1 = 0;
+  go.right2 = PI_RIGHT_DEFAULT;
+  go.leght1 = 0;
+  go.leght2 = PI_LEGHT_DEFAULT;
+
+
+  double d_direction = 0;  // 偏差
+  int integral_counter = 0;  // 積分のカウント数
+  double integral = 0;  // 積分の足し合わせ
+
+  do {
     //ローバー駆動
-    delay(500);
+    rover_analog(go);
+
+//    delay(500);
+    
     gps2 = gps1;
     gps_get(&gps1);
 
@@ -44,15 +58,23 @@ int status5(ROVER *rover) {
     double deg =  rad2deg(acos(vector2d_inner(vct, north_vct)));  // 0~π
 
     // 自己方位の確定
-    if (0 <= vct.x){
+    if (0 <= vct.x) {
       deg = deg;
     } else {
       deg = -deg;
     }
-    double angle = get_angle_devision_gps(deg, gps1.Direction);  // ここで出てくるのが必要な回転角的なやつ
+
+    // 偏差
+    d_direction = get_angle_devision_gps(deg, gps1.Direction);
+    integral += d_direction;  // 積分の増加、リセット等を行う
+    integral_counter += 1;
+    integral = integral * integral_riset(integral_counter);
 
     // 出力調整部分
-    
+    go = get_drive_input(go, d_direction, integral);   // 入力を得る
+    write_control_sd("input analog motor(" + String(go.right1) + " " + String(go.right2) + " " + String(go.leght1) + " " + String(go.leght2) + " " ")");
+
+
 
   } while (1);
 }
